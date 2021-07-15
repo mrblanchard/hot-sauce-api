e568d5f51a8ae7ce399ee055df2516e159e5679c
Jeremy Blanchard <mrjeremyblanchard@gmail.com>
2021-07-14 20:21:28 -0400

    refactor fetch

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,26 @@
 const loadData = async () => {
-  const url = 'https://hot-sauce-api.herokuapp.com/HotSauce';
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(data);
-}
+  try {
+    const url1 = 'https://hot-sauce-api.herokuapp.com/HotSauce/0';
+    const url2 = 'https://hot-sauce-api.herokuapp.com/HotSauce/1';
 
-loadData();
+    const results = await Promise.all([
+      fetch(url1),
+      fetch(url2)
+    ]);
+    const dataPromises = results.map(result => result.json());
+    const finalData = await Promise.all(dataPromises);
+    return finalData;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// top level IIFE method
+(async () => {
+  const data = await loadData();
+  console.log(data);
+})();
+
+// normal method
+// loadData();
+// loadData().then(data => console.log(data));
