c47dcf48f16c60f6cb5cac631328d5a7d2cc4ceb
Jeremy Blanchard <mrjeremyblanchard@gmail.com>
2021-07-18 18:54:43 -0400

    update fetch method

--- public/scripts.js
+++ public/scripts.js
@@ -1,21 +1,38 @@
+fetch('https://hot-sauce-api.herokuapp.com/HotSauce/',{
+  method: 'DELETE',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    name: 'hotsauce'
+  })
+})
+.then(res => {
+  return res.json()
 
+})
 
-    const url = "https://hot-sauce-api.herokuapp.com/HotSauce/0";
-    async function getSauce(){
-    try{
-        const response = await fetch(url);
-        const data = await response.json();
-        const {name, sauce, web, description, contact, email} = data;
-        document.getElementById('name').textContent = name;
-        document.getElementById('sauceN').textContent = sauce;
-        document.getElementById('web').textContent = web;
-        document.getElementById('sdescription').textContent = description;
-        document.getElementById('media').textContent = contact.email;
+.then(data => {
+  return data
+})
 
-      }
-      catch (err) {
-        console.error(err);
-      }
-    }
+.catch(error => console.log('error'))
+    // const url = "https://hot-sauce-api.herokuapp.com/HotSauce/";
+    // async function getSauce(){
+    // try{
+    //     const response = await fetch(url);
+    //     const data = await response.json();
+    //     const {name, sauce, web, description, contact, email} = data;
+    //     document.getElementById('name').textContent = name;
+    //     document.getElementById('sauceN').textContent = sauce;
+    //     document.getElementById('web').textContent = web;
+    //     document.getElementById('sdescription').textContent = description;
+    //     document.getElementById('media').textContent = contact.email;
 
-  getSauce();
+    //   }
+    //   catch (err) {
+    //     console.error(err);
+    //   }
+    // }
+
+  // getSauce();
