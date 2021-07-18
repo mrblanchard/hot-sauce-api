ff713900933c362cee0a53fb5e067da725685725
Jeremy Blanchard <mrjeremyblanchard@gmail.com>
2021-07-18 19:51:51 -0400

    adding button to get sauce

--- public/scripts.js
+++ public/scripts.js
@@ -1,38 +1,26 @@
-fetch('https://hot-sauce-api.herokuapp.com/HotSauce/',{
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    name: 'hotsauce'
-  })
-})
-.then(res => {
-  return res.json()
+document.getElementById('btn').addEventListener('click', getHotSauce);
 
-})
+function getHotSauce() {
 
-.then(data => {
-  return data
-})
+  fetch('https://hot-sauce-api.herokuapp.com/HotSauce/')
+    .then((res) => res.json()) 
+    .then((data) => {
+      let output = `<h2>Hot Sauce API</h2>`;
 
-.catch(error => console.log('error'))
-    // const url = "https://hot-sauce-api.herokuapp.com/HotSauce/";
-    // async function getSauce(){
-    // try{
-    //     const response = await fetch(url);
-    //     const data = await response.json();
-    //     const {name, sauce, web, description, contact, email} = data;
-    //     document.getElementById('name').textContent = name;
-    //     document.getElementById('sauceN').textContent = sauce;
-    //     document.getElementById('web').textContent = web;
-    //     document.getElementById('sdescription').textContent = description;
-    //     document.getElementById('media').textContent = contact.email;
+    data.forEach(function(hotsauce){
+      output += `
+        <ul>
+        <li>Company: ${hotsauce.name}</li>
+        <li>Sauce: ${hotsauce.sauce}</li>
+        <li>Description: ${hotsauce.description}</li>
+        <li>Ingredients: ${hotsauce.ingredients}</li>
+        <li>Heat Level: ${hotsauce.heatlevel}</li>
+        <li>Website: ${hotsauce.web}</li>
+        </ul>
+      `;
+    });
+    document.getElementById('output').innerHTML = output;
+    })
 
-    //   }
-    //   catch (err) {
-    //     console.error(err);
-    //   }
-    // }
-
-  // getSauce();
+    .catch(error => console.log(error));
+}
